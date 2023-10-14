b01bfaf12a0ddeadcd03b9bc02ec674c276a412d
Yushaku <levanson180200@gmail.com>
2023-10-14 20:10:16 +0700

    feat: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,29 @@ const roboto = Roboto({
 
 export const metadata: Metadata = {
   title: 'AI assistents',
-  description: "Generate answer for student's questions"
+  description: "Generate answer for student's questions",
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      url: '/favicon.ico',
+    },
+  ],
 }
 
 export default function RootLayout({
