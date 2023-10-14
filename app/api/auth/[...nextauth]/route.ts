70ac88ce9e229f515cd6ea16d1e7d0a6ccc50d19
Yushaku <levanson180200@gmail.com>
2023-10-14 19:38:58 +0700

    feat: authorazation

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import Google from 'next-auth/providers/google'
+import prisma from '@/lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
@@ -16,41 +19,47 @@ const authOptions: NextAuthOptions = {
   secret: process.env.SECRET,
   session: { strategy: 'jwt' },
   callbacks: {
-    signIn({ user, account, profile }) {
+    signIn({ account, profile }) {
       if (
         account?.provider === 'google' &&
         profile?.email?.endsWith('@huce.edu.vn')
       ) {
-        console.debug(user);
         return true
       } else {
         return false
       }
     },
+    jwt: ({ token, user }) => {
+      if (!user?.email) return token
 
+      const email = user.email
+      const match = email.match(/\d+/);
+
+      const schoolId = match?.at(0)
+      if (!schoolId) return token
+      const isAdmin = schoolId.length <= 3 ? true : false
+
+      return {
+        ...token,
+        isAdmin,
+        id: user.id
+      }
+    },
     session: ({ session, token }) => {
       return {
         ...session,
         user: {
           ...session.user,
           id: token.id,
-          randomKey: token.randomKey
+          randomKey: token.randomKey,
+          isAdmin: token?.isAdmin ?? false
         }
       }
     },
-    jwt: ({ token, user }) => {
-      if (user) {
-        const u = user
-        return {
-          ...token,
-          id: u.id
-        }
-      }
-      return token
-    }
   }
 }
 
 const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
+
