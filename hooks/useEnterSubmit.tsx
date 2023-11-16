a67d4902492ac6b5984bd296953cec04185f9dc9
Yushaku <levanson180200@gmail.com>
2023-11-17 00:34:06 +0700

    add custome hooks

---
+++ hooks/useEnterSubmit.tsx
@@ -0,0 +1,24 @@
+import { type RefObject, useRef } from 'react'
+
+export function useEnterSubmit(): {
+  formRef: RefObject<HTMLFormElement>
+  // eslint-disable-next-line no-unused-vars
+  onKeyDown: (event: React.KeyboardEvent<HTMLTextAreaElement>) => void
+} {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>
+  ): void => {
+    if (
+      event.key === 'Enter' &&
+      !event.shiftKey &&
+      !event.nativeEvent.isComposing
+    ) {
+      formRef.current?.requestSubmit()
+      event.preventDefault()
+    }
+  }
+
+  return { formRef, onKeyDown: handleKeyDown }
+}
